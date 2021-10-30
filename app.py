3cfdd6d8a64ac9078824a778b4ffa2300300ff13
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-10-30 21:34:49 +0900

    pythonServer1.1

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ import json
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
+symData = [];
+
 @app.route('/')
 def welcome():
-    return render_template('index.html')
+    return render_template('index.html', symData)
 
 @app.route('/webhook', methods=['POST'])
 def whatever():
-    print(request.data)
+    
     data = json.loads(request.data)
 
+    symData[0] = data['exchange'];
+    symData[1] = data['price'];
+    symData[2] = data['volume'];
+    symData[3] = data['time'];
     return {
         "code": "success",
         "message": data
