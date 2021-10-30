31b05079d1cac0de66b11ec0fa8c8c2760adf63d
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-10-30 23:23:50 +0900

    pythonServer1.7

--- app.py
+++ app.py
@@ -9,17 +9,6 @@ def welcome():
 
 @app.route('/webhook', methods=['POST'])
 def whatever():
-    data = json.loads(request.data)
-
-    if data['passphrase'] != config.WEBHOOK_PASSPHRASE:
-        return{
-            "code": "error",
-            "message": "Nice try, invalid passphrase"
-        }
-
-    print(data);
-    render_template('dddd.html')
-    return {
-        "code": 'success',
-        "message": data
-    }
+    
+    return render_template('dddd.html')
+    
