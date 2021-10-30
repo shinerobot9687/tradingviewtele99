e9c02951de7207e56d576eb39763a5327f73971b
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-10-30 21:20:09 +0900

    pythonServer1.0

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import json
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def welcome():
+    return render_template('index.html')
 
 @app.route('/webhook', methods=['POST'])
 def whatever():
