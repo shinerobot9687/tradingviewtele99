4921e0194adbec8a26aacbdf0531a6011afcd087
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-10-31 20:47:41 +0900

    final3.2

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def welcome():
+    bot = telegram.Bot(token='2075219807:AAGv_N_NmKXAie0F-bhzOR8woQ7QV-W-_7Y')
+    chat_id = 1000903796
+
     return render_template('index.html')
 
 @app.route('/webhook',methods=['POST'])
@@ -21,9 +24,7 @@ def whatever():
 
     totalString = '종목 : ' + readData['exchange'] + ' 거래량 : ' + str(readData['volume']) + ' 금액 : ' + str(readData['price'])
 
-    bot = telegram.Bot(token='2075219807:AAGv_N_NmKXAie0F-bhzOR8woQ7QV-W-_7Y')
-    chat_id = 1000903796
-
+    
     bot.sendMessage(chat_id=chat_id, text=totalString)
 
     print(readData)
