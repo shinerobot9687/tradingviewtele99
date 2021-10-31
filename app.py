a2221d9011c8cc72ff274680c9ecf63ae257707e
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-10-31 15:50:44 +0900

    server1.6

--- app.py
+++ app.py
@@ -10,39 +10,6 @@ def welcome():
     #data = {"exchange":"FTX",  "price": "1000",  "volume": "6442.4186", "time":"2021-10-30T11:59:18Z"}
     return render_template('index.html')
 
-@app.route('/webhook',methods=['POST'])
-def whatever():
-    readData = json.loads(request.data)
-    
-    #global coinInfo
-    
-    #if coinInfo:
-    #    for i in coinInfo.keys():
-    #        if readData['exchange'] == i:
-    #            checkBool = True
-    #            break
-    #        else : 
-    #            checkBool = False
-        
-    #    if checkBool == True:
-    #        coinInfo[i] = coinInfo[i] + 1
-                
-    #    elif checkBool == False:
-    #        coinInfo.update({readData['exchange'] : 1})
-    #else:
-    #    coinInfo = {readData['exchange'] : 1}
-        
-    totalString = '종목 : ' + readData['exchange'] + '거래량 : ' + readData['volume'] + '금액 : ' + readData['price']
-
-    requests.post("https://slack.com/api/chat.postMessage",
-    headers={"Authorization": "Bearer "+"xoxb-1601840097190-1605530619493-lvOwi9vuMfRXdJ7rXm9T1god"},
-    data={"channel": "#stock","text": totalString})
-
-    
-    return {
-        "code": "succss",
-        "message": readData
-    }
     #return render_template('dddd.html')
     
 #app.run()
