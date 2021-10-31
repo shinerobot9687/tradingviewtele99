282e5c7615e50a6c83e7a968dcdaafb81a570e9b
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-10-31 16:50:58 +0900

    server3.1

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import json
-import os
-#import requests
+import requests
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET'])
 def welcome():
     return render_template('index.html')
+
+@app.route('/webhook',methods=['POST'])
+def whatever():
+    readData = json.loads(request.data)
+
+    totalString = '종목 : ' + readData['exchange'] + '거래량 : ' + readData['volume'] + '금액 : ' + readData['price']
+
+    requests.post("https://slack.com/api/chat.postMessage",
+    headers={"Authorization": "Bearer "+"xoxb-1601840097190-1605530619493-lvOwi9vuMfRXdJ7rXm9T1god"},
+    data={"channel": "#stock","text": totalString})
+    
+    return {
+        "code": "succss",
+        "message": readData
+    }
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
