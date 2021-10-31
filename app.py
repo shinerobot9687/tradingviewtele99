c9901c0b48bd69bbdd9ec9085b6bed8fb36d7890
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-10-31 16:33:49 +0900

    server2.6

--- app.py
+++ app.py
@@ -8,4 +8,6 @@ def welcome():
     return render_template('index.html')
 
 
-app.run()
+ app.run(host='0.0.0.0', port=8000) if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000")
+  app.run(host="0.0.0.0", port=port)
