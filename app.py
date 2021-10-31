d7e4d71f5cf8461e182f3346182463449e845149
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-10-31 16:27:52 +0900

    server2.5

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
+@app.route('/')
 def welcome():
     return render_template('index.html')
 
