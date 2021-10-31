5dc1daa195d159dd35b9baceaa0dfa5e966d8ddd
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-10-31 15:42:02 +0900

    server1.2

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import json
-
+import requests
 from flask import Flask, request, render_template
 app = Flask(__name__)
+
+
+
+
+coinInfo ={}
 
 @app.route('/')
 def welcome():
@@ -10,13 +15,40 @@ def welcome():
 
 @app.route('/webhook',methods=['POST'])
 def whatever():
-    data = json.loads(request.data)
-    print(data);
+    readData = json.loads(request.data)
+    #coinInfo = {readData['exchange'] : 1}
+    global coinInfo
+    
+    if coinInfo:
+        for i in coinInfo.keys():
+            if readData['exchange'] == i:
+                checkBool = True
+                break
+            else : 
+                checkBool = False
+        
+        if checkBool == True:
+            coinInfo[i] = coinInfo[i] + 1
+                
+        elif checkBool == False:
+            coinInfo.update({readData['exchange'] : 1})
+    else:
+        coinInfo = {readData['exchange'] : 1}
+        
+    totalString = '종목 : ' + readData['exchange'] + '거래량 : ' + readData['volume'] + '금액 : ' + readData['price']
+
+    requests.post("https://slack.com/api/chat.postMessage",
+    headers={"Authorization": "Bearer "+"xoxb-1601840097190-1605530619493-lvOwi9vuMfRXdJ7rXm9T1god"},
+    data={"channel": "#stock","text": totalString})
+
+    
     return {
         "code": "succss",
-        "message": data
+        "message": readData
     }
+    #return render_template('dddd.html')
     
+app.run()
 
 #if request.method == 'POST':
 #        data = json.loads(request.data)
