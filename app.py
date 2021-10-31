46d82ef0a3ab6f896dd48d03506a81b7443a1f48
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-10-31 16:00:32 +0900

    server1.9

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import requests
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET', 'POST'])
 def welcome():
     return render_template('index.html')
 
-@app.route('/webhook',methods=['POST'])
+@app.route('/webhook',methods=['GET','POST'])
 def whatever():
     readData = json.loads(request.data)
     return {
