6293916490354493e1f374e009e8d761ed5d5f7a
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-10-31 13:55:33 +0900

    server1.0

--- app.py
+++ app.py
@@ -5,19 +5,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    data = {"exchange":"FTX",  "price": "1000",  "volume": "6442.4186", "time":"2021-10-30T11:59:18Z"}
-    return render_template('index.html', symread = data)
+    #data = {"exchange":"FTX",  "price": "1000",  "volume": "6442.4186", "time":"2021-10-30T11:59:18Z"}
+    return render_template('index.html')
 
-@app.route('/webhook', methods=['POST','GET'])
+@app.route('/webhook',methods=['POST'])
 def whatever():
+    data = json.loads(request.data)
+    print(data);
+    return {
+        "code": "succss",
+        "message": data;
+    }
     
 
-    if request.method == 'POST':
-        data = json.loads(request.data)
-        return {
-            "code": 'success',
-            "message": data
-        }
-    else:
-        return render_template('index.html', symread = data)
-    
+#if request.method == 'POST':
+#        data = json.loads(request.data)
+#        return {
+#            "code": 'success',
+#            "message": data
+#        }
+#    else:
+#        return 
