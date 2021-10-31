a96885510b1bc88ca8d050948414e0d3003643d5
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-10-31 15:58:38 +0900

    server1.8

--- app.py
+++ app.py
@@ -3,22 +3,14 @@ import requests
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-#coinInfo ={}
-
 @app.route('/')
 def welcome():
-    #data = {"exchange":"FTX",  "price": "1000",  "volume": "6442.4186", "time":"2021-10-30T11:59:18Z"}
     return render_template('index.html')
 
-    #return render_template('dddd.html')
-    
-#app.run()
-
-#if request.method == 'POST':
-#        data = json.loads(request.data)
-#        return {
-#            "code": 'success',
-#            "message": data
-#        }
-#    else:
-#        return 
+@app.route('/webhook',methods=['POST'])
+def whatever():
+    readData = json.loads(request.data)
+    return {
+        "code": "succss",
+        "message": readData
+    }
