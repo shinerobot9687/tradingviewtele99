959523313cde102e2038ab50203898238ee5197c
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-11-01 09:48:50 +0900

    count add

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
     
-    
+coinSym = {}
 
 @app.route('/',methods=['GET'])
 def welcome():
@@ -20,15 +20,26 @@ def welcome():
 def whatever():
     readData = json.loads(request.data)
 
-    totalString = '종목 : ' + readData['exchange'] + ' 거래량 : ' + str(readData['volume']) + ' 금액 : ' + str(readData['price'])
+    global coinSym
 
-    bot = telegram.Bot(token='2075219807:AAGv_N_NmKXAie0F-bhzOR8woQ7QV-W-_7Y')
-    #chat_id = 1000903796
-    chat_id = '@cryptotradingviewconnect'
-    bot.sendMessage(chat_id=chat_id, text=totalString)
-    bot.sendMessage(chat_id=1000903796, text=totalString)
+    if coinSym:
+        for coin in coinSym.keys():
+            if coin == readData['exchange']:
+                if coinSym[coin] >= 1:
+                    coinSym[coin] = 0
+                    #print("reset",coinSym)
 
-    print(readData)
+                    totalString = '종목 : ' + readData['exchange'] + ' 거래량 : ' + str(readData['volume']) + ' 금액 : ' + str(readData['price'])
+
+                    bot = telegram.Bot(token='2075219807:AAGv_N_NmKXAie0F-bhzOR8woQ7QV-W-_7Y')
+                    chat_id = '@cryptotradingviewconnect'
+                    bot.sendMessage(chat_id=chat_id, text=totalString)
+                    bot.sendMessage(chat_id=1000903796, text=totalString)
+                else:
+                    coinSym[coin] = coinSym[coin] + 1
+    else:
+        coinSym = {readData['exchange'] : 1}
+        
     return {
         "code": "succss",
         "message": readData
