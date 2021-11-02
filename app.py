9fddf1d6e997d28912767024e747cba9badf5d30
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-11-03 02:44:17 +0900

    bug fixed1.1

--- app.py
+++ app.py
@@ -16,40 +16,43 @@ def welcome():
 def whatever():
     readData = json.loads(request.data)
 
-    global coinSym
-
-    if coinSym:
-        check = False;
-        coinSymRead = readData['exchange'];
-        for coin in coinSym.keys():
-            if coin == coinSymRead:
-                check = True;
-                break;
-        
-        if check == True:
-            if coinSym[coinSymRead] >= 1:
-                coinSym[coinSymRead] = 0
-
-                totalString = readData['name']+' | 코인 : ' + readData['exchange'] + ' 거래량 : ' + str(readData['volume']) + ' 금액 : ' + str(readData['price']) + ' 시간 : ' + str(readData['time'])
-
-                bot = telegram.Bot(token='2075219807:AAGv_N_NmKXAie0F-bhzOR8woQ7QV-W-_7Y')
-                chat_id = '@cryptotradingviewconnect'
-                bot.sendMessage(chat_id=chat_id, text=totalString)
-                bot.sendMessage(chat_id=1000903796, text=totalString)
-
-                print('send',totalString)
-            elif coinSym[coinSymRead] < 1:
-                coinSym[coinSymRead] = coinSym[coinSymRead] + 1
-                print('sum', coinSym[coinSymRead],coinSymRead)
-
-        else:
-            coinSym.update({coinSymRead : 1})
-            print('add', coinSym[coinSymRead], coinSymRead)
-            
+    if readData['arbitrage']:
+        print(readData);
     else:
-        coinSym = {readData['exchange'] : 1}
-        print('add', coinSym[readData['exchange']],readData['exchange'])
-    
+        global coinSym
+
+        if coinSym:
+            check = False;
+            coinSymRead = readData['exchange'];
+            for coin in coinSym.keys():
+                if coin == coinSymRead:
+                    check = True;
+                    break;
+            
+            if check == True:
+                if coinSym[coinSymRead] >= 1:
+                    coinSym[coinSymRead] = 0
+
+                    totalString = readData['name']+' | 코인 : ' + readData['exchange'] + ' 거래량 : ' + str(readData['volume']) + ' 금액 : ' + str(readData['price']) + ' 시간 : ' + str(readData['time'])
+
+                    bot = telegram.Bot(token='2075219807:AAGv_N_NmKXAie0F-bhzOR8woQ7QV-W-_7Y')
+                    chat_id = '@cryptotradingviewconnect'
+                    bot.sendMessage(chat_id=chat_id, text=totalString)
+                    bot.sendMessage(chat_id=1000903796, text=totalString)
+
+                    print('send',totalString)
+                elif coinSym[coinSymRead] < 1:
+                    coinSym[coinSymRead] = coinSym[coinSymRead] + 1
+                    print('sum', coinSym[coinSymRead],coinSymRead)
+
+            else:
+                coinSym.update({coinSymRead : 1})
+                print('add', coinSym[coinSymRead], coinSymRead)
+                
+        else:
+            coinSym = {readData['exchange'] : 1}
+            print('add', coinSym[readData['exchange']],readData['exchange'])
+        
         
 
     #totalString = readData['name']+' 종목 : ' + readData['exchange'] + ' 거래량 : ' + str(readData['volume']) + ' 금액 : ' + str(readData['price'])
