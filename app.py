0940351735f8c382a8027799ca7bd172068ac219
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-11-03 09:40:25 +0900

    last1.15

--- app.py
+++ app.py
@@ -57,7 +57,8 @@ def whatever():
                             totalString = '✅ '+ readData['name']+' | 코인 : ' + readData['exchange'] + ' 거래량 : ' + str(readData['volume']) + ' 금액 : ' + str(readData['price']) + ' 시간 : ' + str(readData['time'])
                     time.sleep(1)
                     bot = telegram.Bot(token='2075219807:AAGv_N_NmKXAie0F-bhzOR8woQ7QV-W-_7Y')
-                    bot.sendMessage(chat_id=1000903796, text=totalString)
+                    chat_id = '1000903796'
+                    bot.sendMessage(chat_id=chat_id, text=totalString)
 
                     print('send',totalString)
                 elif coinSym[coinSymRead] < 1:
